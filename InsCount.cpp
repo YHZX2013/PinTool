db8591a9d8aced25f8f49f259c3b36aef72f8c7e
root <445638271@qq.com>
2017-01-23 18:52:45 +0800

    PinTool

--- InsCount.cpp
+++ InsCount.cpp
@@ -27,44 +27,44 @@ INT32 Usage()
 
 VOID Fini(INT32 code, VOID *v)
 {
-	if (g_log_file_ptr == NULL)
-	{
-		puts("File write error");
-		return;
-	}
+    if (g_log_file_ptr == NULL)
+    {
+	puts("File write error");
+	return;
+    }
 
-	fprintf(g_log_file_ptr, "%u\n", g_u_inscount);
-	map<string, unsigned int>::iterator it;
+    fprintf(g_log_file_ptr, "%u\n", g_u_inscount);
+    map<string, unsigned int>::iterator it;
 
-	for (it = g_map_inscode.begin(); it != g_map_inscode.end(); it++)
-	{
-		fprintf(g_log_file_ptr, "%s:%u\n", it->first.c_str(), it->second);
-	}
+    for (it = g_map_inscode.begin(); it != g_map_inscode.end(); it++)
+    {
+	fprintf(g_log_file_ptr, "%s:%u\n", it->first.c_str(), it->second);
+    }
 
-	fclose(g_log_file_ptr);
-	g_log_file_ptr = NULL;
+    fclose(g_log_file_ptr);
+    g_log_file_ptr = NULL;
 
-	puts("Fini!");
+    puts("Fini!");
 }
 
 VOID InsCount(char *ins)
 {
-	g_map_inscode[ins] ++;
-	g_u_inscount ++;
+    g_map_inscode[ins] ++;
+    g_u_inscount ++;
 }
 
 VOID Instruction(INS ins, VOID *v)
 {
-	IMG img = IMG_FindByAddress(INS_Address(ins));
-	if (IMG_Valid(img) && IMG_IsMainExecutable(img))
-	{
-		string ins_dis = INS_Disassemble(ins);
-		char *ins_tmp = (char*)malloc((ins_dis.length() + 1) * sizeof(char));
+    IMG img = IMG_FindByAddress(INS_Address(ins));
+    if (IMG_Valid(img) && IMG_IsMainExecutable(img))
+    {
+	string ins_dis = INS_Disassemble(ins);
+	char *ins_tmp = (char*)malloc((ins_dis.length() + 1) * sizeof(char));
 
-		sscanf(ins_dis.c_str(), "%s", ins_tmp);
+	sscanf(ins_dis.c_str(), "%s", ins_tmp);
 
-		INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)InsCount, IARG_PTR, ins_tmp, IARG_END); 
-	}
+	INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)InsCount, IARG_PTR, ins_tmp, IARG_END); 
+    }
 }
 
 int main(int argc, char *argv[])
@@ -79,16 +79,15 @@ int main(int argc, char *argv[])
 
     if (index == string::npos)
     {
-	printf("work_dir error! Use %s\n", work_dir);
-	return -1;
+	printf("Work_dir may Use .\\ got %s\n", work_dir);
+    }
+    else
+    {
+	work_dir.assign(work_dir, 0, index);
+	WINDOWS::SetCurrentDirectory(work_dir.c_str());
     }
 
-    work_dir.assign(work_dir, 0, index);
-
-    WINDOWS::SetCurrentDirectory(work_dir.c_str());
- 
     g_log_file_ptr = fopen("inslog.txt", "w");
-	
     if (g_log_file_ptr == NULL)
     {
 	puts("Error open file!");
